f1697cb2c80b2fc00a623730d1b6e92e29b3f603
lleo0h <leonardoaraujo0147@gmail.com>
2025-10-04 01:16:03 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Kaisen Leaderboard",
+  description: "Veja o ranking atualizado em tempo real do kaizenmc.gg",
+  creator: "lleo0h"
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  )
+}
